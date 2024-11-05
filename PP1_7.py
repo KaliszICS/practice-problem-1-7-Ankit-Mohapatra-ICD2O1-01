060bdee4dc58cca6a5abcbe7a5d73218f96b806d
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-11-05 10:18:18 -0500

    Update PP1_7.py

--- PP1_7.py
+++ PP1_7.py
@@ -1,29 +1,29 @@
  
 
 def q1():
-print(True)
+ print(True)
 
 def q2():
-x = int(input("Input an integer: "))
-print(x > 5)
+ x = int(input("Input an integer: "))
+ print(x > 5)
   
 
 def q3():
-xAgain = input("Input the letter a: ")
-if xAgain == "a" or "A" :
+ xAgain = input("Input the letter a: ")
+ if xAgain == "a" or "A" :
   print(True)
-else : 
+ else : 
   print(False)
 
 def q4():
-xx = input( "Input a word earlier in the dictionary than google: ")
-print(xx != "google")
+ xx = input( "Input a word earlier in the dictionary than google: ")
+ print(xx != "google")
 
 def q5():
-var_1 = int(input("Input an integer: "))
-var_2 = int(input("Input another integer: "))
-var_3=(var_1*var_2) >40
-print(f"Your numbers multiplied together are greater than 40, {var_3}")
+ var_1 = int(input("Input an integer: "))
+ var_2 = int(input("Input another integer: "))
+ var_3=(var_1*var_2) >40
+ print(f"Your numbers multiplied together are greater than 40, {var_3}")
 
 #Do edit the code below
 #Comment the lines below when running your tests
